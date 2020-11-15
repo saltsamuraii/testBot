81fd251fcd34bf28b71eebb84255cb5368cf5128
Kirill <example@gmail.com>
2020-11-15 15:54:46 +0300

    test with grid bootstrap

---
+++ app.js
@@ -0,0 +1,14 @@
+/* Burger menu */
+document.getElementById('navToggle').addEventListener('click',function(e){
+    e.preventDefault();
+    if(this.classList.contains('active')) {
+        this.classList.remove('active');
+        document.getElementById('showMenu').classList.remove('active');
+
+    }
+    else {
+        this.classList.add('active');
+        document.getElementById('showMenu').classList.add('active');
+    }
+})
+
